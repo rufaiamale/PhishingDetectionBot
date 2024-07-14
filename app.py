3efcc65314aa3ae62d8ae72d92c01fe800e1ffc4
rufaiamale <rufaiamale@gmail.com>
2024-07-14 21:41:25 +0100

    Added port binding for Heroku deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -50,4 +51,6 @@ def health():
     return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
